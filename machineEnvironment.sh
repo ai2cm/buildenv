bd481b3e97f972f8fc2e94e978e7a8e4b68937eb
Oliver Fuhrer <oli@ginko.ch>
2020-07-22 09:48:34 -0700

    first version of buildenv

---
+++ machineEnvironment.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+# setup environment for different systems
+# 
+# NOTE: the location of the base bash script and module initialization
+#       vary from system to system, so you will have to add the location
+#       if your system is not supported below
+
+exitError()
+{
+    \rm -f /tmp/tmp.${user}.$$ 1>/dev/null 2>/dev/null
+    echo "ERROR $1: $3" 1>&2
+    echo "ERROR     LOCATION=$0" 1>&2
+    echo "ERROR     LINE=$2" 1>&2
+    exit $1
+}
+
+showWarning()
+{
+    echo "WARNING $1: $3" 1>&2
+    echo "WARNING       LOCATION=$0" 1>&2
+    echo "WARNING       LINE=$2" 1>&2
+}
+
+modulepathadd() {
+    if [ -d "$1" ] && [[ ":$MODULEPATH:" != *":$1:"* ]]; then
+        MODULEPATH="${MODULEPATH:+"$MODULEPATH:"}$1"
+    fi
+}
+
+# setup empty defaults
+host=""         # name of host
+queue=""        # standard queue to submit jobs to
+nthreads=""     # number of threads to use for parallel builds
+mpilaunch=""    # command to launch an MPI executable (e.g. aprun)
+installdir=""   # directory where libraries are installed
+testdata=""     # directory where unittestdata is stored
+
+# setup machine specifics
+if [ "`hostname | grep daint`" != "" ] ; then
+    . /etc/bash.bashrc
+    . /opt/modules/default/init/bash
+    . /etc/bash.bashrc.local
+    export host="daint"
+    queue="normal"
+    nthreads=8
+    mpilaunch="srun"
+    installdir=/project/c14/install/${host}
+    testdata=/scratch/snx3000/jenkins/data
+    export CUDA_ARCH=sm_60
+elif [ "`hostname | grep dom`" != "" ] ; then
+    . /etc/bash.bashrc
+    . /opt/modules/default/init/bash
+    . /etc/bash.bashrc.local
+    export host="dom"
+    queue="normal"
+    nthreads=8
+    mpilaunch="srun"
+    installdir=/project/c14/install/${host}
+    testdata=/scratch/snx3000/jenkins/data
+    export CUDA_ARCH=sm_60
+elif [ "`hostname | grep dora`" != "" ] ; then
+    . /etc/bash.bashrc
+    . /opt/modules/default/init/bash
+    export host="dora"
+    queue="normal"
+    nthreads=8
+    mpilaunch="aprun"
+    installdir=/project/c14/install/daint
+    testdata=/scratch/dora/jenkins/data
+## Disabled block for testing a particular kesch node with special environment
+elif [[ "$(hostname)" == "keschcn-0012"* ]]; then
+    . /etc/bashrc && true # In some conditions the omitted true triggered an error.
+    export host="kesch-test"
+    echo "The host is ${host}"
+    queue="debug"
+    nthreads=1
+    mpilaunch="srun"
+    installdir="/project/c14/install/${host}"
+    testdata="/scratch/jenkins/data"
+    export CUDA_ARCH=sm_37
+elif [ "`hostname | grep kesch`" != "" -o "`hostname | grep escha`" != "" ] ; then
+    . /etc/bashrc && true # In some conditions the omitted true triggered an error.
+    if [ "${NODE_NAME}" == kesch-pgi ] ; then
+	export host="kesch-pgi"
+    else
+	export host="kesch"
+    fi
+    queue="debug"
+    nthreads=1
+    mpilaunch="srun"
+    installdir="/project/c14/install/${host}"
+    testdata="/scratch/jenkins/data"
+    export CUDA_ARCH=sm_37
+elif [ "`hostname | grep arolla`" != "" -o "`hostname | grep tsa`" != "" ] ; then
+    . /etc/bashrc
+    export host="tsa"
+    queue="debug"
+    nthreads=1
+    mpilaunch="srun"
+    installdir="/project/c14/install/tsa"
+    testdata="/scratch/jenkins/data"
+    export CUDA_ARCH=sm_70
+fi
+
+# make sure everything is set
+test -n "${host}" || exitError 2001 ${LINENO} "Variable <host> could not be set (unknown machine `hostname`?)"
+test -n "${queue}" || exitError 2002 ${LINENO} "Variable <queue> could not be set (unknown machine `hostname`?)"
+test -n "${nthreads}" || exitError 2003 ${LINENO} "Variable <nthreads> could not be set (unknown machine `hostname`?)"
+test -n "${mpilaunch}" || exitError 2004 ${LINENO} "Variable <mpilaunch> could not be set (unknown machine `hostname`?)"
+test -n "${installdir}" || exitError 2005 ${LINENO} "Variable <installdir> could not be set (unknown machine `hostname`?)"
+
+# export installation directory
+export INSTALL_DIR="${installdir}"
+export TESTDATA_DIR="${testdata}"
+
---
+++ slurmTools.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# SLURM tools
+
+##################################################
+# functions
+##################################################
+
+exitError()
+{
+    \rm -f /tmp/tmp.${user}.$$ 1>/dev/null 2>/dev/null
+    echo "ERROR $1: $3" 1>&2
+    echo "ERROR     LOCATION=$0" 1>&2
+    echo "ERROR     LINE=$2" 1>&2
+    exit $1
+}
+
+showWarning()
+{
+    echo "WARNING $1: $3" 1>&2
+    echo "WARNING       LOCATION=$0" 1>&2
+    echo "WARNING       LINE=$2" 1>&2
+}
+
+# function to launch and wait for job (until job finishes or a
+# specified timeout in seconds is reached)
+#
+# usage: launch_job script timeout
+
+function launch_job {
+  local script=$1
+  local timeout=$2
+  local sacct_maxwait=90
+
+  # check sanity of arguments
+  test -f "${script}" || exitError 7201 ${LINENO} "cannot find script ${script}"
+  if [ -n "${timeout}" ] ; then
+      echo "${timeout}" | grep '^[0-9][0-9]*$' 2>&1 > /dev/null
+      if [ $? -ne 0 ] ; then
+          exitError 7203 ${LINENO} "timeout is not a number"
+      fi
+  fi
+
+  # get out/err of SLURM job
+  local out=`grep '^\#SBATCH --output=' ${script} | sed 's/.*output=//g'`
+  local err=`grep '^\#SBATCH --error=' ${script} | sed 's/.*error=//g'`
+
+  # submit SLURM job
+  local res=`sbatch ${script}`
+  if [ $? -ne 0 ] ; then
+      exitError 7205 ${LINENO} "problem submitting SLURM batch job"
+  fi
+  echo "${res}" | grep "^Submitted batch job [0-9][0-9]*$" || exitError 7206 ${LINENO} "problem determining job ID of SLURM job"
+  local jobid=`echo "${res}" | sed  's/^Submitted batch job //g'`
+  test -n "${jobid}" || exitError 7207 ${LINENO} "problem determining job ID of SLURM job"
+
+  # wait until job has finished (or maximum sleep time has been reached)
+  if [ -n "${timeout}" ] ; then
+      local secs=0
+      local inc=2
+      local job_status="UNKNOWN"
+      while [ $secs -lt $timeout ] ; do
+          echo "...waiting ${inc}s for SLURM job ${jobid} to finish (status=${job_status})"
+          sleep ${inc}
+          secs=$[$secs+${inc}]
+          inc=60
+          squeue_out=`squeue -o "%.20i %.20u %T" -h -j "${jobid}" 2>/dev/null`
+          echo "${squeue_out}" | grep "^ *${jobid} " &> /dev/null
+          if [ $? -eq 1 ] ; then
+              break
+          fi
+          job_status=`echo ${squeue_out} | sed 's/.* //g'`
+      done
+  fi
+
+  # make sure that job has finished
+  squeue_out=`squeue -o "%.20i %.20u %T" -h -j "${jobid}" 2>/dev/null`
+  echo "${squeue_out}" | grep "^ *${jobid} " &> /dev/null
+  if [ $? -eq 0 ] ; then
+      exitError 7207 ${LINENO} "batch job ${script} with ID ${jobid} on host ${slave} did not finish"
+  fi
+
+  # check for normal completion of batch job
+  # Since the slurm data base may take time to update, wait until sacct_maxwait
+  local sacct_wait=0
+  local sacct_inc=30
+  local sacct_log=sacct.${jobid}.log
+  local sacct_status=1
+
+  # XL_HACK: On tsa RH7.6 the job exit with non 0 also the model completed successfully
+  # For the time being we ignore the slurm status (a check is done on the output)
+  if [ -n "${COSMO_IGNORE_SLURM_STATUS}" ]; then
+      echo "!! Warning: slurm status is not checked if COSMO_IGNORE_SLURM_STATUS is set"
+      return
+  fi
+
+  while [ $sacct_wait -lt $sacct_maxwait ] ; do
+      sacct --jobs ${jobid} -p -n -b -D 2>/dev/null > ${sacct_log}
+      # Check that sacct returns COMPLETED
+      grep -v '|COMPLETED|0:0|' ${sacct_log} >/dev/null
+      if [ $? -eq 0 ]; then
+	  echo "Status not COMPLETED, waiting 30s for data base update"
+	  sleep 30
+      else
+	  sacct_status=0
+	  break
+      fi
+      sacct_wait=$[$sacct_wait+${sacct_inc}]
+  done
+
+  if [ $sacct_status -ne 0 ] ; then
+      if [ -n "${out}" ] ; then
+          echo "=== ${out} BEGIN ==="
+          cat ${out} | /bin/sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"
+          echo "=== ${out} END ==="
+      fi
+      if [ -n "${err}" ] ; then
+          echo "=== ${err} BEGIN ==="
+          cat ${err} | /bin/sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"
+          echo "=== ${err} END ==="
+      fi
+      echo "=== ${sacct_log} BEGIN ==="
+      cat ${sacct_log}
+      echo "=== ${sacct_log} END ==="
+      exitError 7209 ${LINENO} "batch job ${script} with ID ${jobid} on host ${slave} did not complete successfully"
+  fi
+  rm ${sacct_log}
+}
+
+

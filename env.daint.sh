13c42c78f434401019f8d30e89de78e3dfa6e9ec
Oliver Fuhrer <oli@ginko.ch>
2020-09-07 20:57:12 -0700

    update modules

--- env.daint.sh
+++ env.daint.sh
@@ -6,4 +6,9 @@
 
 module load daint-gpu
 module load cray-python
-module load nvidia_math_libs/2020_207
+module load Boost
+module load cudatoolkit
+NVCC_PATH=$(which nvcc)
+export CUDA_HOME=$(echo $NVCC_PATH | sed -e "s/\/bin\/nvcc//g")
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+

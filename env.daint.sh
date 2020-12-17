7f28c70e2882260cfa837378fccd75bb55dbfc9d
Eddie Davis <eddied@vulcan.com>
2020-12-16 16:04:08 -0800

    Enable MPICH RDMA for CUDA

--- env.daint.sh
+++ env.daint.sh
@@ -4,11 +4,14 @@
 # environments for the dycore and the Fortran parts. Namely, the following
 # functions must be defined in this file:
 
+module swap PrgEnv-cray PrgEnv-gnu
 module load daint-gpu
 module load cray-python
+module load cray-mpich
 module load Boost
 module load cudatoolkit
 NVCC_PATH=$(which nvcc)
 export CUDA_HOME=$(echo $NVCC_PATH | sed -e "s/\/bin\/nvcc//g")
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+export MPICH_RDMA_ENABLED_CUDA=1
 

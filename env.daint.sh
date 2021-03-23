1129130e126ae4e0f8307d359d513141409e9774
Oliver Fuhrer <oli@ginko.ch>
2021-03-23 15:34:38 -0700

    revert back to gcc/8.1.0 and cuda/10.2

--- env.daint.sh
+++ env.daint.sh
@@ -11,13 +11,17 @@ module load cray-mpich
 module load Boost
 module load cudatoolkit
 module load graphviz
+
+# since gridtools does not play nice with gcc 8.3 we switch to 8.1
+module switch gcc gcc/8.1.0
+
+# we need to work with CUDA 10.2
+module switch cudatoolkit cudatoolkit/10.2.89_3.29-7.0.2.1_3.5__g67354b4
+
 NVCC_PATH=$(which nvcc)
 export CUDA_HOME=$(echo $NVCC_PATH | sed -e "s/\/bin\/nvcc//g")
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 export MPICH_RDMA_ENABLED_CUDA=1
-
-# since gridtools does not play nice with gcc 8.3 we switch to 8.1
-module switch gcc/8.3.0 gcc/8.1.0
 
 # the eve toolchain requires a clang-format executable, we point it to the right place
 export CLANG_FORMAT_EXECUTABLE=/project/s1053/install/venv/vcm_1.0/bin/clang-format

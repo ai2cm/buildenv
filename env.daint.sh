9565eb44bdd292635744e7ba83bc3752b4524d07
Rhea George <rheag@allenai.org>
2022-02-21 10:27:07 -0800

    trying florians setup

--- env.daint.sh
+++ env.daint.sh
@@ -6,7 +6,7 @@
 
 module load daint-gpu
 module swap PrgEnv-cray PrgEnv-gnu
-module load cray-python/3.9.4.1
+module load cray-python/3.8.2.1
 module load cray-mpich/7.7.18
 module load Boost/1.78.0-CrayGNU-21.09-python3
 module load cudatoolkit/11.2.0_3.39-2.1__gf93aa1c
@@ -14,7 +14,8 @@ module load graphviz/2.44.0
 module load /project/s1053/install/modulefiles/gcloud/303.0.0
 module load cray-hdf5
 
-module switch gcc gcc/10.3.0
+module switch gcc gcc/8.3.0
+module switch cray-python cray-python/3.8.2.1
 
 # load gridtools modules
 module load /project/s1053/install/modulefiles/gridtools/1_1_3
